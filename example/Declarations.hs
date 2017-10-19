4617fa1cae2a8b3460c342a3d6bb593c45e2134a
Ilya Zuev <Ilya.Zuev@deutsche-finanzagentur.de>
2017-10-19 17:12:18 +0200

    update example

--- example/Declarations.hs
+++ example/Declarations.hs
@@ -4,14 +4,9 @@ class Cls1 a where fn1 :: a -> a
 
 class Cls2 a where fn2 :: a -> a
 
-
 data A = A deriving Show
-
 instance Cls1 A where fn1 x = x
 
-
 data B = B deriving Show
-
 instance Cls1 B where fn1 x = x
-
 instance Cls2 B where fn2 x = x
--- example/Example.hs
+++ example/Example.hs
@@ -8,16 +8,16 @@ instance InferInstanceBase B
 testData :: [WrappedInstance InferInstanceBase]
 testData = [WrappedInstance A, WrappedInstance B]
 
-isCls1 w = $(wrappedInstanceOf ''Cls1) w
-
-isCls2 w = $(wrappedInstanceOf ''Cls2) w
-
-asCls2 w = $(asWrappedInstanceOf ''Cls2) w
-
 main = do
+    print [w|w<-testData, isCls1 w]
     print [w|w<-testData, isCls2 w]
     print [show$ fn2 a|w<-testData, WrappedInstance a<-[asCls2 w]]
+    where
+        isCls1 w = $(wrappedInstanceOf ''Cls1) w
+        isCls2 w = $(wrappedInstanceOf ''Cls2) w
+        asCls2 w = $(asWrappedInstanceOf ''Cls2) w
 
 -- output:
+--[A,B]
 --[B]
 --["B"]
